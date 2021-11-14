5f23d0d5d39d724554bc6b363f531dfdc3b635f3
Pranav Ajay <pranavajay2002@gmail.com>
2021-11-14 23:22:41 +0530

    Updated drawer elements

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var kdrawerheaderstyle =  TextStyle(color: Colors.black);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/components/tweet.dart';
+import 'package:twitter_clone/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({ Key? key }) : super(key: key);
@@ -13,12 +14,32 @@ class Home extends StatelessWidget {
       ),
       drawer: Drawer(
         child: ListView(
+          // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
+            // ignore: prefer_const_constructors
             UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
+              decoration: BoxDecoration(
+                color: Colors.white
+              ),
+              
+              
+              currentAccountPicture: const CircleAvatar(
                 backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/74711322?v=4'),
               ),
-              accountName: Text('Pranav Ajay'), accountEmail: Text('pranavajay2002@gmail.com')),
+              accountName: Text('Pranav Ajay',style: kdrawerheaderstyle), accountEmail: Text('pranavajay2002@gmail.com',style: kdrawerheaderstyle,)),
+              const ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.featured_play_list),
+                title: Text('Lists'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.flash_on),
+                title: Text('Moments'),
+              ),
+
 
           ],
         ),
