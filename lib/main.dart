afb73b956dd1d85e07d2a808b3e776585b74d53d
Pranav Ajay <pranavajay2002@gmail.com>
2021-11-18 01:04:23 +0530

    Added skeleton to pages

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget { MyApp({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: 'home',
       routes: {
         'home':(context)=> Home(),
